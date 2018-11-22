d178427e4ea24bbc554b824ef3c89ce29d890d0e
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-11-22 11:13:30 +0200

    Initial project

---
+++ LearningSystem/LearningSystem.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿namespace LearningSystem.Web.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => this.View();
+
+        public IActionResult About()
+        {
+            this.ViewData["Message"] = "Your application description page.";
+
+            return this.View();
+        }
+
+        public IActionResult Contact()
+        {
+            this.ViewData["Message"] = "Your contact page.";
+
+            return this.View();
+        }
+
+        public IActionResult Privacy() => this.View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier
+            };
+
+            return this.View(errorViewModel);
+        }
+    }
+}
