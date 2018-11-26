2988c7cecee495f9279cb38cbaab3e8b9b4b2b91
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-11-26 14:55:13 +0200

    Add abstract controller

---
+++ LearningSystem/LearningSystem.Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿namespace LearningSystem.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public abstract class AdminController : Controller
+    {
+       
+    }
+}
---
+++ LearningSystem/LearningSystem.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace LearningSystem.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    
+    public class HomeController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
