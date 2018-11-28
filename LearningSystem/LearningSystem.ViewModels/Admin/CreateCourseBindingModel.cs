f08157006a2019da9c51d83451a9e14a031c7665
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-11-28 11:58:00 +0200

    All ViewModels are now in this layer

---
+++ LearningSystem/LearningSystem.ViewModels/Admin/CreateCourseBindingModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LearningSystem.ViewModels.Admin
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateCourseBindingModel
+    {
+        [Required]
+        [StringLength(200, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public string Slug { get; set; }
+    }
+}
