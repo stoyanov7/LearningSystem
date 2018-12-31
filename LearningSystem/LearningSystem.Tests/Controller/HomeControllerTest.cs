991ac48063cc53d345b6c981935642f3fb1a7460
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-12-31 16:45:40 +0200

    Fix failing test

--- LearningSystem/LearningSystem.Tests/Controller/HomeControllerTest.cs
+++ LearningSystem/LearningSystem.Tests/Controller/HomeControllerTest.cs
@@ -1,22 +1,28 @@
 ﻿namespace LearningSystem.Tests.Controller
 {
+    using System.Threading.Tasks;
+    using LearningSystem.Services.Admin.Contracts;
+    using LearningSystem.Services.Blog.Contracts;
     using Microsoft.AspNetCore.Mvc;
+    using Moq;
     using Web.Areas.Admin.Controllers;
     using Xunit;
 
     public class HomeControllerTest
     {
-        //[Fact]
-        //public void Index_ReturnsTheProperView()
-        //{
-        //    // Arrange
-        //    var controller = new HomeController();
+        [Fact]
+        public async Task Index_ReturnsTheProperView()
+        {
+            // Arrange
+            var blogArticleServiceMock = new Mock<IBlogArticleService>();
+            var adminCourseInstancesServiceMock = new Mock<IAdminCourseInstancesService>();
+            var controller = new HomeController(blogArticleServiceMock.Object, adminCourseInstancesServiceMock.Object);
 
-        //    // Act
-        //    var result = controller.Index();
+            // Act
+            var result = await controller.Index();
 
-        //    // Assert
-        //    Assert.IsType<ViewResult>(result);
-        //}
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
     }
 }
