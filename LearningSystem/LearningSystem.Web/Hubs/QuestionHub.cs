bff5409b3c4de3a54f41997820c7b4d09f26877a
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-12-31 14:40:33 +0200

    New form for Join group and ask question

--- LearningSystem/LearningSystem.Web/Hubs/QuestionHub.cs
+++ LearningSystem/LearningSystem.Web/Hubs/QuestionHub.cs
@@ -2,17 +2,36 @@
 {
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.SignalR;
+    using Models.CourseInstance;
+    using Services.Student.Contracts;
 
     public class QuestionHub : Hub
     {
-        public async Task PostQuestion(string user, string question)
+        private readonly IStudentQuestionsService questionsService;
+
+        public QuestionHub(IStudentQuestionsService questionsService)
         {
-            await this.Clients.All.SendAsync("showQuestion", user, question);
+            this.questionsService = questionsService;
         }
-    
-        public override Task OnConnectedAsync()
+
+        public async Task JoinGroup(string groupName)
         {
-            return base.OnConnectedAsync();
+            var course = await this.questionsService.GetCourseInstanceAsync<CourseInstanceQuestionsViewModel>(groupName);
+
+            if (course == null)
+            {
+                await this.Clients.Caller.SendAsync("group-rejected", "There is no such group");
+            }
+            else
+            {
+                await this.Groups.AddToGroupAsync(this.Context.ConnectionId, groupName);
+                await this.Clients.Caller.SendAsync("group-accept", course.Name, course.Slug);
+            }
+        }
+
+        public async Task AskQuestion(string groupName, string question)
+        {
+            await this.Clients.Group(groupName).SendAsync("receive-question", question);
         }
     }
 }
--- LearningSystem/LearningSystem.Web/wwwroot/js/questions.js
+++ LearningSystem/LearningSystem.Web/wwwroot/js/questions.js
@@ -1,23 +1,48 @@
 ﻿/// <reference path="../lib/signalr/signalr.js" />
 $(function() {
     let baseUrl = "https://localhost:44319/";
-    let connectionBuilder = new signalR.HubConnectionBuilder().withUrl(baseUrl + "questions");
-    let connection = connectionBuilder.build();
+    let group = {
+        name: null,
+        slug: null
+    };
 
-    connection.start()
+    let connection = new signalR.HubConnectionBuilder()
+        .withUrl(baseUrl + "questions")
+        .build();
+
+    connection
+        .start()
         .then(() => {
-            $("#submit-button").click(e => {
+            $("#join-group-form").submit(e => {
                 e.preventDefault();
-                let username = $("#username").val();
-                let question = $("#question").val();
+                let groupName = $("#group-name").val();
+                connection.invoke("JoinGroup", groupName);
+            });
 
-                connection.invoke("PostQuestion", username || "unknown", question);
+            $("#ask-question-form").submit(e => {
+                e.preventDefault();
+                let question = $("#question").val();
+                connection.invoke("AskQuestion", group.slug, question);
             });
             
-            connection.on("showQuestion", (user, question) => {
-                $("#questions").append($("<div>").html(`<strong>${user}</strong>: ${question}`));
+            connection.on("group-accept", (courseName, slug) => {
+                group.name = courseName;
+                group.slug = slug;
+
+                $("#join-group-form").hide();
+                $("#ask-question-form").show();
+
+                $("#questions")
+                    .append($("<div>").html(`<h2>${courseName}</h2>`))
+                    .append($("<div>").attr("id", "questions-container"));
             });
-        }, err => console.log(err));
+
+            connection.on("group-rejected", reason => console.log(reason));
+
+            connection.on("receive-question", question =>
+                $("#questions-container").append($("<div>").text(question)));
+        })
+        .catch(err => console.log(err));
 
     console.log(connection);
 });
