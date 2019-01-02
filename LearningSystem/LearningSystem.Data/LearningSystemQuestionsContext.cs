1d319c4199d76ebc2f8f5f62d883b5599a2a2891
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-01-03 00:27:33 +0200

    Create questions context #9

--- LearningSystem/LearningSystem.Data/LearningSystemQuestionsContext.cs
+++ LearningSystem/LearningSystem.Data/LearningSystemQuestionsContext.cs
@@ -7,8 +7,11 @@
         public LearningSystemQuestionsContext()
         {
             this.Client = new MongoClient("mongodb://localhost:27017");
+            this.Database = this.Client.GetDatabase("LearningSystemQuestions");
         }
 
         public MongoClient Client { get; set; }
+
+        public IMongoDatabase Database { get; set; }
     }
 }
