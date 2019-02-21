d75ea32f63ff64a429d0845065a76d81919ecfcd
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-02-21 15:27:13 +0200

    Add dispose method #16

--- LearningSystem/LearningSystem.Repository/UnitOfWork.cs
+++ LearningSystem/LearningSystem.Repository/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿namespace LearningSystem.Repository
 {
+    using System;
     using System.Threading.Tasks;
     using Contracts;
     using Microsoft.EntityFrameworkCore;
@@ -7,24 +8,44 @@
     public class UnitOfWork<TContext> : IUnitOfWork<TContext>
         where TContext : DbContext
     {
+        private bool disposed = false;
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="UnitOfWork"/> class.
+        /// Initializes a new instance of the <see cref="UnitOfWork{T}"/> class.
         /// </summary>
-        public UnitOfWork(TContext context)
-        {
-            this.Context = context;
-        }
+        public UnitOfWork(TContext context) => this.Context = context;
 
         /// <summary>
         /// Getter for context.
         /// </summary>
         public TContext Context { get; }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                this.Context.Dispose();
+            }
+
+            // Free any unmanaged objects here.
+            this.disposed = true;
+        }
+
         /// <inheritdoc />
         /// <summary>
         /// Releases the allocated resources for this context. 
         /// </summary>
-        public void Dispose() => this.Context.Dispose();
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
         /// <summary>
         /// Asynchronous save all changes made in this context to the database.
